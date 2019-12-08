4545de1ba119684131d6735412b409df84f5cf19
ruanpienaar <ruan800@gmail.com>
2019-12-08 16:36:53 +0000

    http call WIP

---
+++ src/erlang_testing_web.erl
@@ -0,0 +1,54 @@
+-module(erlang_testing_web).
+
+-export([
+    url_req/1,
+    long_url_req/1,
+    close_long_req/1
+]).
+
+url_req(Url) ->
+    case
+        holster:once_off_req(
+            Url,
+            #{
+                retries => 0,
+                retry_timeout => 0,
+                domain_lookup_timeout => 250,
+                connect_timeout => 250,
+                http_opts => #{
+                    closing_timeout => 250
+                }
+            })
+    of
+        {response, {StatusCode, ResponseHeaders, ResponseData}} ->
+            {ok, {StatusCode, ResponseHeaders, ResponseData}};
+        {response, Response} ->
+            {error, Response};
+        X ->
+            X
+    end.
+
+long_url_req(Url) ->
+    case
+        holster:long_running_req(
+            Url,
+            #{
+                retry => 10000,
+                retry_timeout => 0,
+                domain_lookup_timeout => 250,
+                connect_timeout => 250,
+                http_opts => #{
+                    closing_timeout => 250
+                }
+            }
+        )
+    of
+    %% Here we might want to monitor this pid...
+        {{ok, Pid}, {response, {StatusCode, ResponseHeaders, ResponseData}}} ->
+            {ok, Pid, {StatusCode, ResponseHeaders, ResponseData}};
+        {{ok, Pid}, {response, Response}} ->
+            {ok, Pid, Response}
+    end.
+
+close_long_req(Pid) ->
+    holster:close_req(Pid).
